5d38199ec57420dfbadf6ab7343a01549c6c273d
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-03-30 19:07:22 +0530

    cart service update

--- demo/src/main/java/com/example/FoodDelivery/Services/CartService.java
+++ demo/src/main/java/com/example/FoodDelivery/Services/CartService.java
@@ -4,7 +4,6 @@ import com.example.FoodDelivery.Repository.CartRepo;
 import com.example.FoodDelivery.Repository.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.*;
 @Service
 public class CartService {
