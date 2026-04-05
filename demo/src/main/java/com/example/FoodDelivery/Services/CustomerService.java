7a0cf9434e8dae948ca9c664c61a81bc435d91a6
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-04-05 19:43:30 +0530

    CustomerService updated

--- demo/src/main/java/com/example/FoodDelivery/Services/CustomerService.java
+++ demo/src/main/java/com/example/FoodDelivery/Services/CustomerService.java
@@ -3,7 +3,6 @@ import com.example.FoodDelivery.Entities.*;
 import com.example.FoodDelivery.Repository.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 @Service
 public class CustomerService {
